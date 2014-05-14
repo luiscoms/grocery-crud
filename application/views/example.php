072da00e6ef3dbca511291dc7012cde0eb32c5fe
John Skoumbourdis <scoumbourdisj@gmail.com>
2014-05-15 00:29:37 +0300

    Show Twitter bootstrap V2 at the examples

--- application/views/example.php
+++ application/views/example.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-<?php 
+<?php
 foreach($css_files as $file): ?>
 	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 <?php endforeach; ?>
@@ -31,15 +31,15 @@ a:hover
 		<a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
 		<a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> |
 		<a href='<?php echo site_url('examples/products_management')?>'>Products</a> |
-		<a href='<?php echo site_url('examples/offices_management')?>'>Offices</a> | 
-		<a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> |		 
-		<a href='<?php echo site_url('examples/film_management')?>'>Films</a> | 
-		<a href='<?php echo site_url('examples/film_management_with_extras')?>'>Films With Extras</a> | 
-		<a href='<?php echo site_url('examples/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a> | 
+		<a href='<?php echo site_url('examples/offices_management')?>'>Offices</a> |
+		<a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> |
+		<a href='<?php echo site_url('examples/film_management')?>'>Films</a> |
+		<a href='<?php echo site_url('examples/film_management_with_extras')?>'>Films With Extras</a> |
+		<a href='<?php echo site_url('examples/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a> |
 		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a>
-		
+
 	</div>
-	<div style='height:20px;'></div>  
+	<div style='height:20px;'></div>
     <div>
 		<?php echo $output; ?>
     </div>
